88e146ee68eda5c986e92e9b90fa10420b81a2a4
Laver33 <pad666444@gmail.com>
2026-06-15 11:59:54 +0300

    first commit

---
+++ backend/src/models/postSchema.js
@@ -0,0 +1,20 @@
+
+const postSchema = new mongoose.Schema({    
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})

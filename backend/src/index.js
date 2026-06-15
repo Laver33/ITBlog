fd534a031032d8a33f137c0a702874df3018d604
Laver33 <pad666444@gmail.com>
2026-06-15 12:18:51 +0300

    feat: backend connect bd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 import express, { json } from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const port = 5000;
 
+mongoose.connect(process.env.MONGODB_CONNECT_PORT
+).then(() =>{
+    console.log('База подключена')}
+).catch((err) => {
+    console.log('Проблема с подключением к бд: ', err )})
+
 app.use(express.json())
+
 
 app.listen(port, () => {
     console.log('Server стартовал на: ' + `http://localhost:${port}`)
